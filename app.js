41f413be1d9287b8f3156cb69c270712a8d33e5a
Tim Wheeler <tim@timwheeler.com>
2018-05-27 20:14:21 +0000

    Twitch API v1.1.2

--- app.js
+++ app.js
@@ -1,131 +1,289 @@
-// GET https://api.twitch.tv/kraken/streams/<channel ID>
+var twitchBox = {
 
-var streamBox = document.getElementById('streamBox');
-var streamers = document.getElementsByClassName('streamers');
+    users: ['ninja', 'summit1g', 'OverwatchLeague', 'freecodecamp'],
 
-var users = ['ninja', 'freecodecamp', 'summit1g', 'OverwatchLeague'];
+    streamBox: document.getElementById('streamBox'),
+    streamers: document.getElementsByClassName('streamers'),
+    addUserInput: document.getElementById('addUserInput'),
+    addUserBtn: document.getElementById('addUserBtn'),
+    toolTips: document.getElementsByClassName('tooltip'),
+    watchLive: document.getElementsByClassName('watchLive'),
 
-var addUserInput = document.getElementById('addUserInput');
-var addUserBtn = document.getElementById('addUserBtn');
-var toolTip = document.getElementsByClassName('tooltip');
+    // displayToolTip: function() {
+    //     var toolTips = document.getElementsByClassName('tooltip');
+    //     for (var i in twitchBox.users) {
+    //         toolTips[i].style.display = 'inline-block';
+    //     }
 
-// addUserBtn.addEventListener('click', function(e) {
-//     e.preventDefault();
-//     streamBox.innerHTML = '';
-//     users.push(addUserInput.value);
-//     startApp();
-// });
+    // },
 
-function startApp() {
+    // hideToolTip: function() {
+    //     var toolTips = document.getElementsByClassName('tooltip');
+    //     for (var i in twitchBox.users) {
+    //         toolTips[i].style.display = 'none';
+    //     }
 
-    for (var i = 0; i < users.length; i++) {
-
-        var url = "https://api.twitch.tv/kraken/streams/" + users[i];
-
-        var xhr = new XMLHttpRequest();
-
-        xhr.open('GET', url, true);
-
-        xhr.setRequestHeader("Client-ID", "e7vwg9cc1ocxvimfgff2dvzc6fd81d");
-
-        xhr.onload = function() {
-
-            var data = JSON.parse(this.response);
-
-            // console.log(users[i])
-
-            if (data.stream === null) {
-
-                console.log('user is offline');
-                var li = document.createElement('li');
-                li.innerHTML = users[i];
-                var watch = document.createElement('i');
-                var iconDiv = document.createElement('div');
-                var icon = document.createElement('i');
-
-                li.classList.add('streamers');
-                watch.classList.add('watchLive');
-                watch.classList.add('fab');
-                watch.classList.add('fa-twitch');
-                watch.classList.add('hide');
-
-                iconDiv.classList.add('icon');
-                iconDiv.classList.add('offline');
-                icon.classList.add('fas');
-                icon.classList.add('fa-circle-notch');
-
-                li.appendChild(watch);
-                li.appendChild(iconDiv);
-                iconDiv.appendChild(icon);
-                streamBox.appendChild(li);
-            }
-
-            else {
-
-                var li = document.createElement('li');
-                li.innerHTML = data.stream.channel.display_name;
-                var iconDiv = document.createElement('div');
-                var link = document.createElement('a');
-                var watch = document.createElement('i');
-                var toolTip = document.createElement('div');
-                var icon = document.createElement('i');
+    // },
 
 
-                li.classList.add('streamers');
-                link.href = 'https://twitch.tv/' + data.stream.channel.display_name;
-                link.setAttribute('target', '_blank');
+    // addUserBtn.addEventListener('click', function(e) {
+    //     e.preventDefault();
+    //     streamBox.innerHTML = '';
+    //     users.push(addUserInput.value);
+    //     startApp();
+    // });
 
-                watch.classList.add('watchLive');
-                watch.classList.add('fab');
-                watch.classList.add('fa-twitch');
-                toolTip.style.display = 'none';
+    startApp: function() {
 
+        for (var i in twitchBox.users) {
 
-                toolTip.classList.add('tooltip');
-                toolTip.innerHTML = data.stream.channel.status;
+            var url = "https://api.twitch.tv/kraken/streams/" + twitchBox.users[i];
 
-                iconDiv.classList.add('icon');
-                iconDiv.classList.add('online');
+            var xhr = new XMLHttpRequest();
 
-                icon.classList.add('fas');
-                icon.classList.add('fa-circle-notch');
-                icon.classList.add('fa-spin');
+            xhr.open('GET', url, true);
 
-                function displayToolTip() {
-                    var toolTips = document.getElementsByClassName('tooltip');
-                    for (var i in users) {
-                        toolTips[i].style.display = 'inline-block';
-                    }
+            xhr.setRequestHeader("Client-ID", "e7vwg9cc1ocxvimfgff2dvzc6fd81d");
+
+            xhr.onload = function() {
+
+                var userName = twitchBox.users;
+
+                console.log(twitchBox.users[i]);
+
+                debugger;
+
+                var data = JSON.parse(this.response);
+
+                // console.log(users[i])
+
+                if (data.stream === null) {
+
+                    var offLi = document.createElement('li');
+                    var offWatch = document.createElement('i');
+                    var offIconDiv = document.createElement('div');
+                    var offIcon = document.createElement('i');
+
+                    offLi.classList.add('streamers');
+                    offWatch.classList.add('watchLive');
+                    offWatch.classList.add('fab');
+                    offWatch.classList.add('fa-twitch');
+                    offWatch.classList.add('hide');
+
+                    offIconDiv.classList.add('icon');
+                    offIconDiv.classList.add('offline');
+
+                    offIcon.classList.add('fas');
+                    offIcon.classList.add('fa-circle-notch');
+
+                    offLi.innerHTML = userName[i];
+                    offLi.appendChild(offWatch);
+                    offLi.appendChild(offIconDiv);
+                    offIconDiv.appendChild(offIcon);
+                    twitchBox.streamBox.appendChild(offLi);
 
                 }
 
-                function hideToolTip() {
-                    var toolTips = document.getElementsByClassName('tooltip');
-                    for (var i in users) {
-                        toolTips[i].style.display = 'none';
-                    }
+                else {
 
+                    var li = document.createElement('li');
+                    var iconDiv = document.createElement('div');
+                    var link = document.createElement('a');
+                    var watch = document.createElement('i');
+                    var toolTip = document.createElement('div');
+                    var icon = document.createElement('i');
+
+                    li.classList.add('streamers');
+                    li.innerHTML = data.stream.channel.display_name;
+                    link.href = 'https://twitch.tv/' + data.stream.channel.display_name;
+                    link.setAttribute('target', '_blank');
+
+                    watch.classList.add('watchLive');
+                    watch.classList.add('fab');
+                    watch.classList.add('fa-twitch');
+                    toolTip.style.display = 'none';
+
+
+                    toolTip.classList.add('tooltip');
+                    toolTip.innerHTML = data.stream.channel.status;
+
+                    iconDiv.classList.add('icon');
+                    iconDiv.classList.add('online');
+
+                    icon.classList.add('fas');
+                    icon.classList.add('fa-circle-notch');
+                    icon.classList.add('fa-spin');
+
+                    link.appendChild(watch);
+                    li.appendChild(link);
+                    li.appendChild(toolTip);
+                    li.appendChild(iconDiv);
+                    iconDiv.appendChild(icon);
+                    twitchBox.streamBox.appendChild(li);
+
+                    // console.log(data.stream.channel.status);
                 }
+            };
 
-                watch.onmouseover = displayToolTip;
-                watch.onmouseout = hideToolTip;
+            xhr.send();
 
-                link.appendChild(watch);
-                li.appendChild(link);
-                li.appendChild(toolTip);
-                li.appendChild(iconDiv);
-                iconDiv.appendChild(icon);
-                streamBox.appendChild(li);
-                // console.log(data.stream.channel.display_name, 'is online');
-                // console.log('Watch live:', data.stream._links.self);
-                console.log(data.stream.channel.status);
-            }
         }
 
-        xhr.send();
+    },
 
-    }
+    showToolTip: function() {
+        twitchBox.streamBox.addEventListener('mouseover', function(e) {
+            for (var i in twitchBox.watchLive) {
+                if (e.target.classList.contains('watchLive')) {
+                    e.toolTip[i].style.display = 'block';
+                    console.log('yup');
+                }
+            }
+        })
+    },
 
-}
 
-startApp();
+
+};
+
+twitchBox.startApp();
+twitchBox.showToolTip();
+
+
+
+
+
+
+
+// // GET https://api.twitch.tv/kraken/streams/<channel ID>
+
+
+// var streamBox = document.getElementById('streamBox');
+// var streamers = document.getElementsByClassName('streamers');
+
+// var users = ['ninja', 'freecodecamp', 'summit1g', 'OverwatchLeague'];
+
+// var addUserInput = document.getElementById('addUserInput');
+// var addUserBtn = document.getElementById('addUserBtn');
+// var toolTip = document.getElementsByClassName('tooltip');
+
+// function displayToolTip() {
+//     var toolTips = document.getElementsByClassName('tooltip');
+//     for (var i in users) {
+//         toolTips[i].style.display = 'inline-block';
+//     }
+
+// }
+
+// function hideToolTip() {
+//     var toolTips = document.getElementsByClassName('tooltip');
+//     for (var i in users) {
+//         toolTips[i].style.display = 'none';
+//     }
+
+// }
+
+
+// // addUserBtn.addEventListener('click', function(e) {
+// //     e.preventDefault();
+// //     streamBox.innerHTML = '';
+// //     users.push(addUserInput.value);
+// //     startApp();
+// // });
+
+// function startApp() {
+
+//     for (var i = 0; i < users.length; i++) {
+
+//         var url = "https://api.twitch.tv/kraken/streams/" + users[i];
+
+//         var xhr = new XMLHttpRequest();
+
+//         xhr.open('GET', url, true);
+
+//         xhr.setRequestHeader("Client-ID", "e7vwg9cc1ocxvimfgff2dvzc6fd81d");
+
+//         xhr.onload = function() {
+
+//             var data = JSON.parse(this.response);
+
+//             // console.log(users[i])
+
+//             if (data.stream === null) {
+
+//                 console.log('user is offline');
+//                 var li = document.createElement('li');
+//                 li.innerHTML = users[i];
+//                 var watch = document.createElement('i');
+//                 var iconDiv = document.createElement('div');
+//                 var icon = document.createElement('i');
+
+//                 li.classList.add('streamers');
+//                 watch.classList.add('watchLive');
+//                 watch.classList.add('fab');
+//                 watch.classList.add('fa-twitch');
+//                 watch.classList.add('hide');
+
+//                 iconDiv.classList.add('icon');
+//                 iconDiv.classList.add('offline');
+//                 icon.classList.add('fas');
+//                 icon.classList.add('fa-circle-notch');
+
+//                 li.appendChild(watch);
+//                 li.appendChild(iconDiv);
+//                 iconDiv.appendChild(icon);
+//                 streamBox.appendChild(li);
+//             }
+
+//             else {
+
+//                 var li = document.createElement('li');
+//                 li.innerHTML = data.stream.channel.display_name;
+//                 var iconDiv = document.createElement('div');
+//                 var link = document.createElement('a');
+//                 var watch = document.createElement('i');
+//                 var toolTip = document.createElement('div');
+//                 var icon = document.createElement('i');
+
+
+//                 li.classList.add('streamers');
+//                 link.href = 'https://twitch.tv/' + data.stream.channel.display_name;
+//                 link.setAttribute('target', '_blank');
+
+//                 watch.classList.add('watchLive');
+//                 watch.classList.add('fab');
+//                 watch.classList.add('fa-twitch');
+//                 toolTip.style.display = 'none';
+
+
+//                 toolTip.classList.add('tooltip');
+//                 toolTip.innerHTML = data.stream.channel.status;
+
+//                 iconDiv.classList.add('icon');
+//                 iconDiv.classList.add('online');
+
+//                 icon.classList.add('fas');
+//                 icon.classList.add('fa-circle-notch');
+//                 icon.classList.add('fa-spin');
+
+//                 watch.onmouseover = displayToolTip;
+//                 watch.onmouseout = hideToolTip;
+
+//                 link.appendChild(watch);
+//                 li.appendChild(link);
+//                 li.appendChild(toolTip);
+//                 li.appendChild(iconDiv);
+//                 iconDiv.appendChild(icon);
+//                 streamBox.appendChild(li);
+
+//                 // console.log(data.stream.channel.status);
+//             }
+//         }
+
+//         xhr.send();
+
+//     }
+
+// }
+
+// startApp();
